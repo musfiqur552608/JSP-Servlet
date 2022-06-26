951428302e33b049ed7c97a5a7572241077818dd
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-06-27 01:06:01 +0600

    RequestDispatcher

--- src/main/java/com/musfiq/AddServlet.java
+++ src/main/java/com/musfiq/AddServlet.java
@@ -3,6 +3,8 @@ package com.musfiq;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,12 +19,16 @@ public class AddServlet extends HttpServlet{
 		out.println("result is "+ k);
 	}
 	
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
-		PrintWriter out = res.getWriter();
-		out.println("result is "+ k);
+		k=k*k;
+		
+		req.setAttribute("k", k);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
 	}
 }
