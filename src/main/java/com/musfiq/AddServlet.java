74403f38bb5bf83a1109cdbaada3bcd1b905f07d
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-06-27 01:48:31 +0600

    Cookies

--- src/main/java/com/musfiq/AddServlet.java
+++ src/main/java/com/musfiq/AddServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -32,8 +33,10 @@ public class AddServlet extends HttpServlet{
 //		RequestDispatcher rd = req.getRequestDispatcher("sq");
 //		rd.forward(req, res);
 		
-		HttpSession session = req.getSession();
-		session.setAttribute("k", k);
+//		HttpSession session = req.getSession();
+//		session.setAttribute("k", k);
+		Cookie cookie = new Cookie("k", k + "");
+		res.addCookie(cookie);
 		
 		
 		res.sendRedirect("sq?k="+k);
--- src/main/java/com/musfiq/SqServlet.java
+++ src/main/java/com/musfiq/SqServlet.java
@@ -3,6 +3,7 @@ package com.musfiq;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,9 +11,18 @@ import jakarta.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
-		HttpSession session = req.getSession();
-		int k = (int)session.getAttribute("k");
+//		HttpSession session = req.getSession();
+//		int k = (int)session.getAttribute("k");
 //		int k = Integer.parseInt(req.getParameter("k"));
+		int k=0;
+		Cookie cookies[] = req.getCookies();
+		
+		for(Cookie c: cookies) {
+			if(c.getName().equals("k")) {
+				k = Integer.parseInt(c.getValue());
+			}
+		}
+		
 		PrintWriter out = res.getWriter();
 		out.println("Result is "+k);
 	}
